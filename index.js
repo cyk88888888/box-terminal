ecc6ac39b932761b0dde68cfd2f7598486249777
cyk <935765353@qq.com>
2022-01-10 10:05:14 +0800

    terminal工具

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 #!/usr/bin/env node
 
-const program = require('commander')
-const fs = require('fs')
-const path = require('path')
-const excel = require('./lib/langToJs')
-const xlsxVersion = "0.0.0";// 这里设置项目中xlsx的使用版本
+const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+const excel = require('./lib/langToJs');
+const water = require('./lib/waterMark');
+const getLang = require('./lib/getChinese');
 
 //解析xlsx为多语言js文件
 program.command('xlsx')
-    .description('将box_cfg下对应版本目录lang.xlsx解析位多语言js文件')
+    .description('必须cd到box_client/uni-app下执行命令，将box_cfg下对应版本目录lang.xlsx解析为多语言js文件')
     .action(() => {
-        let xlsxRoot = process.cwd().split("box_client")[0] + "box_cfg" + path.sep + xlsxVersion + path.sep + "lang";
-        fs.access(xlsxRoot, (err) => {
-            if (err) {
-                console.error(`${xlsxRoot}目录不存在`);
-                process.exit();
-            } else {
-                excel.tojs(xlsxRoot, (err) => {
-                    if (err) {
-                        console.error(err)
-                    } else {
-                        console.log('finish!')
-                    }
-                })
-            }
-        });
+        excel.doAction();
     })
 
 //给图片加水印
 program.command('water')
     .description('给图片加水印')
     .action(() => {
-        console.log(11111111111);
+        water.doAction();
     })
 
+// 中文收集并生成key对应中文的json文件  i18n-cli getlang uni-app -f zh.json -d pages,components
+program.command('getlang')
+    .description('对当前目录下的 .js .vue .json .php文件进行中文收集，默认当前目录下面所有文件\n')
+    .option('-f, --filename <filename>', '[optional]设置生成的文件名，默认为 zh_cn.json，需为 .json 文件\n')
+    .option('-d, --ignoredir <ignoredir>', '[optional]需要收集中文的文件夹，默认当前文件夹所有文件', value => {
+        return value.split(',')
+    })
+    .action(({filename = 'zh_cn.json', ignoredir}) => {
+        getLang.doAction(filename, ignoredir);
+    });
 
 program.on('command:*', function () {
     console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
@@ -42,5 +39,4 @@ program.on('command:*', function () {
 if (process.argv.length === 2) {
     program.help();
 }
-
 program.parse(process.argv);
